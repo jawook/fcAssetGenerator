ee9a4658edf87642e4789585c3041948205fc956
jawook <95268438+jawook@users.noreply.github.com>
2025-08-30 19:19:52 -0600

    Update fcAssetGenerator.py

--- fcAssetGenerator.py
+++ fcAssetGenerator.py
@@ -124,6 +124,7 @@ def to_pdf_bytes_flat(poster_img):
 poster = None
 st.set_page_config(page_title="Poster Generator", page_icon="🖼️", layout="centered")
 
+st.image(logo_img)
 st.title("Generate your Own Event Details Poster")
 st.write("""Enter the relevant information below and then click on 'Generate 
          Poster.'""")
